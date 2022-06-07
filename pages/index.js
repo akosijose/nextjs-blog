38860b5daee9d4b8cd3052e75b32f305a75def7d
akosijose <josegeraldlumbao0307@gmail.com>
2022-06-07 09:22:37 +0800

    updating external link

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,10 @@ export default function Home({ allPostsData }) {
         </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn" target="_blank">
+            our Next.js tutorial
+          </a>
+          .)
         </p>
       </section>
 
